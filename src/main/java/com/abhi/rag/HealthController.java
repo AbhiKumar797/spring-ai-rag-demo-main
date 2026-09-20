7662a2139bbbd4eb01a2d5a96fb9172229f57d7a
Abhi-kumar-hub <abhiofficial797@gmai.com>
2026-09-20 23:22:35 +0530

    initial setup file for healthcontroller

---
+++ src/main/java/com/abhi/rag/HealthController.java
@@ -0,0 +1,13 @@
+package com.abhi.rag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Spring AI RAG application is running";
+    }
+}
