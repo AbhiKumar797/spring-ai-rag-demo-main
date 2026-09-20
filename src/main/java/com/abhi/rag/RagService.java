375b46bde8e48219985d1ecf8addadcfcda110ab
Abhi-kumar-hub <abhiofficial797@gmai.com>
2026-09-20 23:44:15 +0530

    i=updated pom.xml

---
+++ src/main/java/com/abhi/rag/RagService.java
@@ -0,0 +1,11 @@
+package com.abhi.rag;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RagService {
+
+    public String processQuery(String query) {
+        return "RAG processing will be implemented here.";
+    }
+}
